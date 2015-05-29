1110d894be127fc64176577b9c22546141f5040e
claude51315 <claude51315@gmail.com>
2015-05-29 13:46:17 +0200

    add read write example

---
+++ lab7/readH.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<termios.h>
+#include<stdlib.h>
+#include<sys/ioctl.h>
+int main()
+{
+	struct termios toptions ; 
+	int fd ; 
+	fd = open("/dev/ttyS3" , O_RDWR | O_NOCTTY);
+	if(fd < 0 )
+	{
+		printf("open error \n");
+		return -1; 
+	}
+	speed_t brate = 9600 ;
+	cfsetispeed(&toptions , brate);
+	cfsetospeed(&toptions  , brate );
+	while(1)	
+	{
+		char buf[2];
+		int i = read(fd , buf , 1 );
+		if(i> 0)
+		{
+			printf("%s\n" , buf);
+		}
+	}
+	close(fd); 
+	return 0 ;
+}
